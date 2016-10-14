6ffb7cc3768f730c3d013789814bb24e5d56f3ef
ryoheiii <ryohei273+github@email.com>
2016-10-14 22:08:02 +0900

    changed .gitignore

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## パス設定
 * yasnippets: loads/inits/20-ysnippets.el
 * auto-complete: loads/inits/20-auto-complete.el
-* migemo: loads/init/20-migemo へインストールディレクトリ内の辞書パス設定
+* migemo: loads/inits/20-migemo.el へインストールディレクトリ内の辞書パス設定
 
 ## font
 * sudo apt-get install fonts-inconsolata
---
+++ loads/my-functions/convenience.el
@@ -0,0 +1,57 @@
+;;; 文字列
+;;; 2012-03-24
+
+;; 文字列の先頭と最後にある一連の空白をすべて削除する
+;; 2012-03-24
+(defun string-strip (string)
+  (replace-regexp-in-string "\\`[ \r\n]*\\|[ \r\n]*\\'" "" string))
+
+
+;;; パス
+;;; 2011-03-19
+
+;; directoryの中にbase-names内のパスが含まれていたらその絶対パスを返す。
+;; 含まれていなかったらdirectoryの親のディレクトリを再帰的に探す。
+;; 2011-03-19
+(defun find-path-in-parents (directory base-names)
+  (or (find-if 'file-exists-p
+               (mapcar (lambda (base-name)
+                         (concat directory base-name))
+                       base-names))
+      (if (string= directory "/")
+          nil
+        (let ((parent-directory (substring directory 0 -1)))
+          (find-path-in-parents parent-directory base-names)))))
+
+;;; window resize
+;; 2015-01-26
+(global-set-key "\C-c\C-r" 'my-window-resizer)
+
+(defun my-window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        c)
+    (catch 'end-flag
+      (while t
+        (message "size[%dx%d]"
+                 (window-width) (window-height))
+        (setq c (read-char))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (message "Quit")
+               (throw 'end-flag t)))))))
