3aea2ee79821742df9dd7a56509db1eb5c7f2a66
Ryohei Matsumoto <ryohei273+github@email.com>
2020-04-26 15:57:06 +0900

    Modify: Config font if needed

--- loads/inits/00-font.el
+++ loads/inits/00-font.el
@@ -1,13 +1,16 @@
-;; sudo apt install fonts-ricty-diminished
-(set-face-attribute 'default nil
-                    :family "Ricty Diminished Discord"
-                    :height 150)
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  (cons "Ricty Diminished Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0212
-                  (cons "Ricty Diminished Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-                  'katakana-jisx0201
-                  (cons "Ricty Diminished Discord" "iso10646-1"))
+(if window-system
+    (progn
+      ;; sudo apt install fonts-ricty-diminished
+      (set-face-attribute 'default nil
+                          :family "Ricty Diminished Discord"
+                          :height 150)
+      (set-fontset-font (frame-parameter nil 'font)
+                        'japanese-jisx0208
+                        (cons "Ricty Diminished Discord" "iso10646-1"))
+      (set-fontset-font (frame-parameter nil 'font)
+                        'japanese-jisx0212
+                        (cons "Ricty Diminished Discord" "iso10646-1"))
+      (set-fontset-font (frame-parameter nil 'font)
+                        'katakana-jisx0201
+                        (cons "Ricty Diminished Discord" "iso10646-1"))
+      ))
