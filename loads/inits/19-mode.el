53f652380d041cc70c0d8b53360a9aa63f883120
Ryohei Matsumoto <ryohei273+github@email.com>
2025-02-09 15:48:20 +0900

    Modify 19-mode.el settings

--- loads/inits/19-mode.el
+++ loads/inits/19-mode.el
@@ -1,4 +1,10 @@
-;;; hs-minor-mode（関数折りたたみ）の設定
+;;; 19-mode.el --- 各種モード設定 -*- lexical-binding: t; -*-
+;;; Commentary:
+;; 主要なメジャーモードの設定を行う
+
+;;; Code:
+
+;;;;;; [Group] Code Folding - コード折りたたみ ;;;;;;
 (use-package hideshow
   :bind (("C-\\" . hs-toggle-hiding)
          ("<f5>" . hs-toggle-hiding))
@@ -8,30 +14,26 @@
          (lisp-mode       . hs-minor-mode))
   )
 
-;;; emacs-lisp-mode
+;;;;;; [Group] Programming Modes - プログラミングモード ;;;;;;
+;;; Elisp-mode - elisp-mode の設定
 (use-package elisp-mode
   :mode ("\\.el\\'" . emacs-lisp-mode)
   :hook (emacs-lisp-mode . my/emacs-lisp-mode-setup)
   :config
   (defun my/emacs-lisp-mode-setup ()
-    "Emacs Lispモード用の設定"
-    ;; タブではなくスペースを用いたインデントを使用
-    (setq indent-tabs-mode nil)
-    ;; インデントを自動化
-    (electric-indent-mode 1)
-    ;; 対応する括弧をハイライト
-    (show-paren-mode 1)
-    ;; コメントやアンコメントの操作を容易にするキーバインドの設定
+    "Emacs Lisp モード用の設定。"
+    (setq indent-tabs-mode nil)  ; タブではなくスペースを使用
+    (electric-indent-mode 1)     ; インデントを自動化
+    (show-paren-mode 1)          ; 括弧の対応をハイライト
     (local-set-key (kbd "C-c ;") 'comment-or-uncomment-region)
-    ;; subwordモードの有効化
-    (subword-mode 1))
+    (subword-mode 1))            ; CamelCase も単語として移動
   )
 
-;;; cc-mode の設定
+;;; Cc-mode - cc-mode の設定
 (use-package cc-mode
   :mode (("\\.C\\'"    . c-mode)
          ;; ("\\.cc\\'"   . cc-mode)
-         ("\\.cc\\'"   . c-mode)
+         ("\\.cc\\'"   . c-mode)  ;; cc-mode ではなく c-mode に統一
          ("\\.nut\\'"  . c++-mode)
          ("\\.cpp\\'"  . c++-mode)
          ("\\.hh\\'"   . c++-mode)
@@ -44,52 +46,33 @@
          (c++-mode      . my/c++-mode-setup))
   :config
   (defun my/cc-mode-setup ()
-    "C/C++ モード共通の設定"
-    ;; コンパイル
-    (local-set-key (kbd "C-c c") 'compile)
-    ;; 自動改行と連続する空白の一括削除を有効にする
-    (c-toggle-auto-hungry-state 1)
-    ;; 他のエディタがファイルを書き換えたらすぐに反映する
-    (auto-revert-mode)
-    ;; インデント設定
+    "C/C++ モード共通の設定。"
+    (local-set-key (kbd "C-c c") 'compile)  ; コンパイル
+    (c-toggle-auto-hungry-state 1)          ; 自動改行 & 連続スペース一括削除
+    (auto-revert-mode)                      ; 外部変更の即時反映
     (setq indent-tabs-mode nil
           c-basic-offset 4))
 
   (defun my/c++-mode-setup ()
-    "C++ モード固有の設定"
+    "C++ モード固有の設定。"
+    ;; 追加設定があればここに記述
     )
   )
 
-;;; text-mode
+;;;;;; [Group] Text Editing - テキスト編集 ;;;;;;
 (use-package text-mode
   :mode (("\\.txt\\'" . text-mode)
-         ("\\.tmp\\'" . text-mode)) ; コミットログ記載時にスニペットを展開したいため
+         ("\\.tmp\\'" . text-mode))
   :hook (text-mode . my/text-mode-setup)
   :config
   (defun my/text-mode-setup ()
-    "テキストモード用の設定"
-    ;; インデント設定
+    "テキストモード用の設定。"
     (setq indent-tabs-mode nil
           tab-width 2
           c-basic-offset 2)
-    ;; subwordモードの有効化
-    (subword-mode 1))
+    (setq tab-stop-list (number-sequence 4 120 4)) ; タブストップを 4 の倍数で設定
+    (subword-mode 1))                              ; CamelCase も単語として移動
   )
 
-;;; txt-mode
-;; txt-mode-common-hook の設定（カスタムフック）
-(add-hook 'txt-mode-common-hook
-          (lambda ()
-            ;; インデント設定
-            (setq indent-tabs-mode nil
-                  tab-width 2
-                  c-indent-level 2
-                  c-basic-offset 2)
-            ;; 自動改行と連続する空白の一括削除を有効にする
-            (c-toggle-auto-hungry-state 1)
-            (c-toggle-hungry-state 1)
-            ;; タブストップリストの設定
-            (setq tab-stop-list (number-sequence 4 120 4)) ; 4 8 12 16...
-            ;; subwordモードの有効化
-            (subword-mode 1))
-          )
+(provide '19-mode)
+;;; 19-mode.el ends here
