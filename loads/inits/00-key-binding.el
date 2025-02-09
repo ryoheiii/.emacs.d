cdf8266444448b28a3bf72103b545c8f05808239
Ryohei Matsumoto <ryohei273+github@email.com>
2025-02-09 15:36:59 +0900

    Modify 00-built-in-package.el settings

--- loads/inits/00-key-binding.el
+++ loads/inits/00-key-binding.el
@@ -1,21 +1,30 @@
-;;; キーバインド設定
-;; C-h でバックスペース
+;;; 00-key-bindings.el --- キーバインド設定 -*- lexical-binding: t; -*-
+;;; Commentary:
+;; Emacs のキーバインド設定
+
+;;; Code:
+
+;;;;;; [Group] Basic Keybindings - 基本キーバインド ;;;;;;
+;; C-h をバックスペースに変更
 (keyboard-translate ?\C-h ?\C-?)
 
-;; 基本的なキーバインド
-(global-set-key (kbd "C-c a") 'align)
-(global-set-key (kbd "C-c M-a") 'align-regexp)
-(global-set-key (kbd "C-c d")   'delete-indentation)
-(define-key global-map (kbd "M-?") 'help-for-help)                 ; ヘルプ
-(define-key global-map (kbd "C-c C-i") 'hippie-expand)             ; 補完
-(define-key global-map (kbd "C-c ;") 'comment-or-uncomment-region) ; コメントアウト
-(define-key global-map (kbd "C-.") 'goto-line)                     ; 指定行へ移動
-(define-key global-map (kbd "M-f") 'forward-symbol)
-(global-set-key [f7] 'toggle-truncate-lines)                       ; 折り返しの有効無効切替
-(global-set-key (kbd "C-c 0") 'my/copy-file-name)
+;; 汎用的な操作
+(global-set-key (kbd "C-c a") 'align)                       ; 文字列の揃え
+(global-set-key (kbd "C-c M-a") 'align-regexp)              ; 正規表現での整列
+(global-set-key (kbd "C-c d") 'delete-indentation)          ; インデント削除
+(global-set-key (kbd "M-?") 'help-for-help)                 ; ヘルプ
+(global-set-key (kbd "C-c C-i") 'hippie-expand)             ; 補完
+(global-set-key (kbd "C-c ;") 'comment-or-uncomment-region) ; コメントアウト
+(global-set-key (kbd "C-.") 'goto-line)                     ; 指定行へ移動
+(global-set-key (kbd "M-f") 'forward-symbol)                ; シンボル単位の移動
+(global-set-key [f7] 'toggle-truncate-lines)                ; 折り返しの有効無効切替
+(global-set-key (kbd "C-c 0") 'my/copy-file-name)           ; ファイル名コピー
 
-;;; ウィンドウ操作に関するキーバインド
-(global-set-key "\C-c\C-r" 'window-resizer)                     ; ウィンドウリサイズ
-(define-key global-map (kbd "M-p") 'other-window-or-split)      ; 次のウィンドウへ移動
-;; (define-key global-map (kbd "C-t") 'next-multiframe-window)     ; 次のウィンドウへ移動
-;; (define-key global-map (kbd "C-q") 'previous-multiframe-window) ; 前のウィンドウへ移動
+;;;;;; [Group] Window Management - ウィンドウ操作 ;;;;;;
+(global-set-key (kbd "C-c C-r") 'window-resizer)            ; ウィンドウサイズ調整
+(global-set-key (kbd "M-p") 'other-window-or-split)         ; 次のウィンドウへ移動
+;; (global-set-key (kbd "C-t") 'next-multiframe-window)     ; 次のフレームのウィンドウへ移動
+;; (global-set-key (kbd "C-q") 'previous-multiframe-window) ; 前のフレームのウィンドウへ移動
+
+(provide '01-key-bindings)
+;;; 00-key-bindings.el ends here
